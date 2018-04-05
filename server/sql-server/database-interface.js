b55fd71d863c3a59792713a73c113cb38749bb94
mandula8 <filipmandic26@gmail.com>
2018-04-05 12:11:19 +0200

    with just third query

--- server/sql-server/database-interface.js
+++ server/sql-server/database-interface.js
@@ -7,8 +7,7 @@ var database = function() {
     var insertUserQuery = require('./db-interface-impl/insert-new-user');
     var confirmUserQuery = require('./db-interface-impl/confirm-user');
 	var selectingSalt = require('./db-interface-impl/selecting-salt');
-    var changePasswordQuery = require('./db-interface-impl/change-password');
-	var mysql = require('mysql');
+    var mysql = require('mysql');
 
 	methods = {
         connection : mysql.createConnection({
@@ -39,11 +38,6 @@ var database = function() {
 		selectingSalt(methods.connection, username, callback);
     };
 
-    methods.changePassword = function(username, oldHash, newHash, newSalt, callback) {
-
-        changePasswordQuery(methods.connection, username, oldHash, newHash, newSalt, callback);
-    };
-
     //-------------------------------------------------------------------------
     //-------------------------------------------------------------------------
 
@@ -59,4 +53,4 @@ var database = function() {
    	return methods;
 }
 
-module.exports = database;
+module.exports = database();
