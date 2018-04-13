3af48c0c00f8837af9567581f3a8f67cefe75ae5
mandula8 <filipmandic26@gmail.com>
2018-04-13 14:59:28 +0200

    deleted confirm user query and fixed change username query

--- server/sql-server/db-interface-impl/change-username.js
+++ server/sql-server/db-interface-impl/change-username.js
@@ -8,24 +8,37 @@ const RESULT = 0;
 
 var changeUsernameCallback = function(info) { return function(error, rows, fields) {
     if (!!error) {
-        console.log("error: query which checks inserts new username failed\n");
-        throw error;
+        info.connection.rollback(function() {
+            console.log("error: query which checks inserts new username failed\n");
+            throw error;
+        });
     }
-    else if (info.callback) info.callback("Success");
+    else {
+        info.connection.commit(function(error) {
+            if (!!error) {
+                info.connection.rollback(function() {
+                    console.log("error: transaction could not be commited, transaction rollback!\n");
+                    throw error;
+                });
+            }
+            else {
+                if (info.callback) info.callback("Success");
+            }
+        });
+    }
 }}
 
 var newUsernameCheckCallback = function(info) { return function(error, rows, fields) {
     if (!!error) {
-        console.log("error: query which checks if newUsername is used failed!\n");
-        throw error;
+        info.connection.rollback(function() {
+            console.log("error: query which checks if newUsername is used failed!\n");
+            throw error;
+        });
     }
     else {
         if (!rows.length) {
-            if (info.extractedPassword === info.passwordHash) {
-                info.connection.query(queries.setNewUsername,
-                    [info.newUsername, info.id], changeUsernameCallback(info));
-            }
-            else if (info.callback) info.callback("PasswordNoMatch");
+            info.connection.query(queries.setNewUsername,
+                [info.newUsername, info.id], changeUsernameCallback(info));
         }
         else if (info.callback) info.callback("UsernameTaken");
     }
@@ -38,22 +51,27 @@ var oldUsernameCheckCallback = function(info) { return function(error, rows, fie
     }
     else {
         if (!!rows.length) {
-            info.extractedPassword = rows[RESULT].password_hash;
             info.id = rows[RESULT].id;
-            info.connection.query(queries.searchInUserByUsername,
-                [info.newUsername], newUsernameCheckCallback(info));
+            info.connection.beginTransaction(function(error) {
+                if (!!error) {
+                    console.log("error: transaction failed to be started!\n");
+                    throw error;
+                }
+                info.connection.query(queries.searchInUserByUsername,
+                    [info.newUsername], newUsernameCheckCallback(info));
+            });
+
         }
         else if (info.callback) info.callback("UserNotRegistered");
     }
 }}
 
-var changeUsername = function(connection, oldUsername, newUsername, hash, callback) {
+var changeUsername = function(connection, oldUsername, newUsername, callback) {
 
     info = {
         connection: connection,
         oldUsername: oldUsername,
         newUsername: newUsername,
-        passwordHash: hash,
         callback: callback
     }
 
