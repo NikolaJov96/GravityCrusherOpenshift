28d30ffc40d1cd0bd7119908bbcb6d6c2f031ee8
Andrija Cicović <cicovic.andrija@gmail.com>
2018-04-14 14:51:54 +0200

    Coding convention fixes in several files. Also, hard-coded port numbers replaced with file-configured port numbers.

--- server/http-server/http-server.js
+++ server/http-server/http-server.js
@@ -4,27 +4,29 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var appConfig = require('../../app-config.js');
 
 // list of expected get request URLs and response files
 var requests = require('./defined-requests.js');
 
 httpListener = function(path){
-	// define callbacks for http get request responses
-	for (var i in requests){
-		app.get(requests[i].URL, function(ind){
-			return function(req, res){
-				res.sendFile(path + requests[ind].file);
-			};
-		}(i));
-	}
+    // define callbacks for http get request responses
+    for (var i in requests){
+        app.get(requests[i].URL, function(ind){
+            return function(req, res){
+                res.sendFile(path + requests[ind].file);
+            };
+        }(i));
+    }
 
-	// set static path for client resource files
-	app.use(express.static(path + '/client'));
+    // set static path for client resource files
+    app.use(express.static(path + '/client'));
 
-	// listen on a port
-	http.listen(8000);
+    // listen on a port
+    http.listen(appConfig.httpServerPort);
 
-	console.log('HTTP get request listener set up.');
+    console.log('HTTP get request listener set up.');
 }
 
 module.exports = httpListener;
+
