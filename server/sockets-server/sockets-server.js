f6ce0b65a0de6567cbd2de3e457c6116da72a9d4
Nikola Jovanovic <jovanovicn.96@gmail.com>
2018-04-18 01:57:20 +0200

    Game rooms step duration telemetry added.

--- server/sockets-server/sockets-server.js
+++ server/sockets-server/sockets-server.js
@@ -25,10 +25,25 @@ io.on('connection', function(socket){
 console.log('Socket.io request listener set up.');
 
 // server loop stepping all active game rooms
+const frameTime = 40;  // run every 40ms (25fps)
+
+// timing telemetry
+var accumulatedTime = 0.0;
+var counter = 0;
+const maxCounter = 60 * 1000 / frameTime;
+
 setInterval(function(){
+    var startTime = process.hrtime();
     var i = serverState.gameRooms.length;
     while (i--){
         var done = serverState.gameRooms[i].step();
         if (done) delete serverState.gameRooms[i];
     }
-}, 40);  // run every 40ms
+    var diffTime = process.hrtime(startTime);
+    accumulatedTime += diffTime[0] * 1e9 + diffTime[1];
+    if (++counter === maxCounter){
+        console.log('Average game rooms step time: ' + accumulatedTime / maxCounter + ' ns');
+        counter = 0;
+        accumulatedTime = 0.0;
+    }
+}, frameTime);
