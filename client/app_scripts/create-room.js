a24882c284a1f18a9f0a5a2d5e0217780d8fcf77
Nikola Jovanovic <jovanovicn.96@gmail.com>
2018-04-18 21:40:46 +0200

    Added opponent attribute and invalid opponent error handler on create room page.

--- client/app_scripts/create-room.js
+++ client/app_scripts/create-room.js
@@ -4,16 +4,18 @@
 
 var roomName = document.getElementById('roomName');
 var submitBtn = document.getElementById('submitBtn');
+var opponent = document.getElementById('inputPlayer2Name');
 
 // send create room request
 submitBtn.onclick = function(){
     if (!socket.connected){ logMsg('Server not yet connected.'); return; }
     
     if (roomName.value.length === 0){ logMsg('Room name field empti.'); roomName.focus(); }
+    else if (opponent.value.length === 0){ logMsg('Opponent name field empti.'); opponent.focus(); }
     else {
         var createGameRoomPkg = {
             name:roomName.value,
-            opponent:'',
+            opponent:opponent.value,
             chatEnabled:true,
             spectatorsEnabled:true,
             gameMap:0
@@ -27,6 +29,9 @@ socket.on('createGameRoomResponse', function(data){
     if (!('status' in data)) attrMissing('status', 'createGameRoomResponse', data);
     
     if (data.status === 'Success'){ logMsg('On createGameRoomResponse - success'); window.location = 'game'; }
-    else if (data.status === 1) logMsg('On createGameRoomResponse - invalid opponent');
-    else logMsg('On createGameRoomResponse - unknown error: ' + data.status);
+    else if (data.status === 'InvalidOpponent'){
+        logMsg('On createGameRoomResponse - invalid opponent');
+        opponent.select();
+        opponent.focus();
+    }else logMsg('On createGameRoomResponse - unknown error: ' + data.status);
 });
