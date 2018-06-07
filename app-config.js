4e46f227adb38a0970fe737bc873e15e57bae444
Nikola Jovanović <NikolaJov96@users.noreply.github.com>
2018-06-08 01:25:15 +0200

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -5,7 +5,7 @@
 var appConfig = {
     webAppProtocol: 'http',
     domainName: 'http://gravity-crusher-psi.193b.starter-ca-central-1.openshiftapps.com',
-    httpServerPort: OPENSHIFT_NODEJS_PORT,
+    httpServerPort: process.env.OPENSHIFT_NODEJS_PORT || 8080,
     socketsServerPort: 8001,
     passwordHashAlgorithm: 'sha512',
     passwordSaltLengthInBytes: 128
